0b84603e7aa4555729eb8d6da2dda7373e55498a
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 17:38:37 +1100

    changed some error messages of the input boxes

--- form.js
+++ form.js
@@ -3,12 +3,25 @@ const username = document.querySelector('#username')
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const confirm = document.querySelector('#password2')
+const formControl = document.querySelector('.form-control')
+
+function showError(target, message) {
+    const formControl = target.parentElement
+    formControl.classList.add('error')
+}
+
+function showSuccess(object, message) {
+    const formControl = target.parentElement
+    formControl.classList.add('success')
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+    if (!username.value) {
+        showError(username, "Username is required")
+    } else {
+        showSuccess(username);
+    }
 })
 
 
-username.addEventListener('change', function () {
-
-})
