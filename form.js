aba56862f6e4cc995135017860c4c7e9b1345690
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 17:24:42 +1100

    prevented default behaviour of the form: submitting

--- form.js
+++ form.js
@@ -1,5 +1,14 @@
+const form = document.querySelector('form')
 const username = document.querySelector('#username')
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const confirm = document.querySelector('#password2')
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+})
+
+
+username.addEventListener('change', function () {
+
+})
