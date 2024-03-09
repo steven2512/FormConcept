c0b0d79dd28ad91c52797bb5a95e55f93fd9e145
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 18:08:05 +1100

    started to refactor js codes

--- form.js
+++ form.js
@@ -32,26 +32,7 @@ function validEmailCheck(email) {
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if (!username.value) {
-        showError(username, "Username is required")
-    } else {
-        showSuccess(username, "");
-    }
-    if (!email.value) {
-        showError(email, "Email is required")
-    } else {
-        showSuccess(email, "");
-    }
-    if (!password.value) {
-        showError(password, "Password is required")
-    } else {
-        showSuccess(password, "");
-    }
-    if (!password2.value) {
-        showError(password2, "Password re-enter is required")
-    } else {
-        showSuccess(password2, "");
-    }
+
 })
 
 
