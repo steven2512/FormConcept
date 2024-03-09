5cb2bfc67c38e932fae1b74be3013ca7331a48b7
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-03-09 17:22:14 +1100

    some minor changes

--- form.js
+++ form.js
@@ -2,3 +2,4 @@ const username = document.querySelector('#username')
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const confirm = document.querySelector('#password2')
+
